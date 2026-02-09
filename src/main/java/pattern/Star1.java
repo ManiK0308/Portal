4a72231272cef592ee1381a3d3d4f97d9efd41dc
manikanta killamsetti <manikillam0308@gmail.com>
2026-02-09 07:38:42 +0530

    new branch static methods

---
+++ src/main/java/pattern/Star1.java
@@ -0,0 +1,25 @@
+package pattern;
+
+public class Star1 {
+public static void main(String[] args) {
+	
+	int stars = -1;
+	int gaps = 4;
+	for (int i=1; i<=4; i++) 
+		{
+		stars+= 2;
+		gaps-= 1;
+		{
+		for(int j =1; j<=gaps;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int k=1; k<=stars; k++)
+		{
+			System.out.print("*");
+		}}
+		
+		System.out.println();
+	}
+}
+}
